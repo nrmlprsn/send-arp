c3de945cad3edf495dd49356f78f19c55794ff01
nrmlprsn <143049879+nrmlprsn@users.noreply.github.com>
2026-04-07 17:38:48 +0900

    Edit main.cpp

--- main.cpp
+++ main.cpp
@@ -37,47 +37,49 @@ int main(int argc, char* argv[]){
 		return EXIT_FAILURE;
 	}
 
-	eth_arp_hdr packet;
+	for(int i=0;2*i<argc;i++){
+		eth_arp_hdr packet;
 	
-	packet.eth.smac = Mac::get_mac(dev);
-	packet.eth.dmac = Mac("FF:FF:FF:FF:FF:FF"); // broadcast
-	packet.eth.type = htons(eth_hdr::ARP);
-	
-	packet.arp.htype = htons(arp_hdr::ETHER);
-	packet.arp.ptype = htons(eth_hdr::IP4);
-	packet.arp.hlen = Mac::Size;
-	packet.arp.plen = Ip::Size;
-	packet.arp.op = arp_hdr::Request;
-	packet.arp.smac = packet.eth.smac;
-	packet.arp.sip = Ip::get_ip(dev);
-	packet.arp.tmac = Mac("00:00:00:00:00:00");
-	packet.arp.tip = Ip(argv[i]); // Sender ip
-
-	if(!send_packet(pcap, &packet)) return -1;
-
-	while(true){
-		struct pcap_pkthdr* header;
-		const u_char* packet_ans;
-		int res = pcap_next_ex(pcap, &header, &packet_ans);
-		if(res == 0) continue;
-		if(res == PCAP_ERROR || res = PCAP_ERROR_BREAK){
-			printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(pcap));
-			break;
-		}
-
-		eth_hdr* eth_ans = (eth_hdr*)packet_ans;
-		if(ntohs(eth_ans->type) != eth_hdr::ARP) continue;
+		packet.eth.smac = Mac::get_mac(dev);
+		packet.eth.dmac = Mac("FF:FF:FF:FF:FF:FF"); // broadcast
+		packet.eth.type = htons(eth_hdr::ARP);
 		
-		arp_hdr* arp_ans = (arp_ans*)(packet_ans + sizeof(eth_hdr));
-		if(ntohs(arp_ans->op != arp_hdr::Reply)) continue;
-
-		if(arp_ans->spa == my_ip){
-			packet.eth.dmac = arp_ans->smac;
-			packet.arp.tmac = arp_ans->smac;
-			break;
+		packet.arp.htype = htons(arp_hdr::ETHER);
+		packet.arp.ptype = htons(eth_hdr::IP4);
+		packet.arp.hlen = Mac::Size;
+		packet.arp.plen = Ip::Size;
+		packet.arp.op = arp_hdr::Request;
+		packet.arp.smac = packet.eth.smac;
+		packet.arp.sip = Ip::get_ip(dev);
+		packet.arp.tmac = Mac("00:00:00:00:00:00");
+		packet.arp.tip = Ip(argv[i]); // Sender ip
+	
+		if(!send_packet(pcap, &packet)) return -1;
+	
+		while(true){
+			struct pcap_pkthdr* header;
+			const u_char* packet_ans;
+			int res = pcap_next_ex(pcap, &header, &packet_ans);
+			if(res == 0) continue;
+			if(res == PCAP_ERROR || res = PCAP_ERROR_BREAK){
+				printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(pcap));
+				break;
+			}
+	
+			eth_hdr* eth_ans = (eth_hdr*)packet_ans;
+			if(ntohs(eth_ans->type) != eth_hdr::ARP) continue;
+			
+			arp_hdr* arp_ans = (arp_ans*)(packet_ans + sizeof(eth_hdr));
+			if(ntohs(arp_ans->op != arp_hdr::Reply)) continue;
+	
+			if(arp_ans->spa == my_ip){
+				packet.eth.dmac = arp_ans->smac;
+				packet.arp.tmac = arp_ans->smac;
+				break;
+			}
 		}
+		packet.arp.sip = Ip(argv[i*2+1]); // Target ip
 	}
-	packet.arp.sip = Ip(argv[i*2+1]); // Target ip
 	
 	pcap_close(pcap);
 
